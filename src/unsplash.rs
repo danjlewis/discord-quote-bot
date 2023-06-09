8e4112a0c92a275557a71884f4044a3ab12f451a
Dan Lewis <48622577+danjlewis@users.noreply.github.com>
2023-06-09 23:38:24 +0100

    refactor `unsplash.rs`

--- src/unsplash.rs
+++ src/unsplash.rs
@@ -5,37 +5,92 @@ use std::{collections::HashMap, io::Cursor};
 use anyhow::{anyhow, Result};
 use reqwest::{
     header::{self, HeaderMap, HeaderValue},
-    Client,
+    Client, Url,
 };
 use serde::{Deserialize, Serialize};
 
-const API_BASE_URL: &str = "https://api.unsplash.com";
-
-fn unsplash_reqwest_client(access_key: &str) -> Client {
-    let mut headers = HeaderMap::new();
-    headers.insert("Accept-Version", HeaderValue::from_static("v1"));
-    headers.insert(
-        header::AUTHORIZATION,
-        format!("Client-ID {access_key}")
-            .try_into()
-            .expect("access_key should not contain invalid header characters"),
-    );
-
-    Client::builder()
-        .default_headers(headers)
-        .build()
-        .expect("reqwest client should build successfully")
+pub struct UnsplashClient {
+    reqwest_client: Client,
+    unsplash_reqwest_client: Client,
+    unsplash_base_url: Url,
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct GetRandomPhotoOptions {
-    pub collections: Option<String>,
-    pub topics: Option<String>,
-    pub username: Option<String>,
-    pub orientation: Option<Orientation>,
+impl UnsplashClient {
+    pub fn new(access_key: &str) -> Self {
+        let reqwest_client = Client::builder()
+            .build()
+            .expect("reqwest client should build successfully");
+
+        let unsplash_reqwest_client = Self::unsplash_reqwest_client(access_key);
+
+        let unsplash_base_url = Url::parse("https://api.unsplash.com")
+            .expect("Unsplash base URL should parse successfully");
+
+        Self {
+            reqwest_client,
+            unsplash_reqwest_client,
+            unsplash_base_url,
+        }
+    }
+
+    fn unsplash_reqwest_client(access_key: &str) -> Client {
+        let mut headers = HeaderMap::new();
+        headers.insert("Accept-Version", HeaderValue::from_static("v1"));
+        headers.insert(
+            header::AUTHORIZATION,
+            format!("Client-ID {access_key}")
+                .try_into()
+                .expect("access_key should not contain invalid header characters"),
+        );
+
+        Client::builder()
+            .default_headers(headers)
+            .build()
+            .expect("Unsplash reqwest client should build successfully")
+    }
+
+    // TODO: implement proper error handling
+    pub async fn get_random_photo(&self, options: GetRandomPhotoOptions) -> Result<DynamicImage> {
+        let metadata: HashMap<String, JsonValue> = self
+            .unsplash_reqwest_client
+            .get(
+                self.unsplash_base_url
+                    .join("/photos/random")
+                    .expect("Unsplash random photo endpoint URL should parse correctly"),
+            )
+            .query(&options)
+            .send()
+            .await?
+            .error_for_status()?
+            .json()
+            .await?;
+
+        let url = metadata
+            .get("urls")
+            .ok_or(anyhow!("Metadata missing `urls` key"))?
+            .get("regular")
+            .ok_or(anyhow!("Metadata missing `urls.regular` key"))?
+            .as_str()
+            .ok_or(anyhow!("Invalid data type of `urls.regular` metadata key"))?;
+
+        let image_data = self
+            .reqwest_client
+            .get(url)
+            .send()
+            .await?
+            .error_for_status()?
+            .bytes()
+            .await?;
+
+        let image = ImageReader::new(Cursor::new(image_data))
+            .with_guessed_format()?
+            .decode()?;
+
+        Ok(image)
+    }
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone, Copy, PartialEq, Eq, Hash)]
 #[serde(rename_all = "lowercase")]
 pub enum Orientation {
     Landscape,
@@ -43,33 +98,10 @@ pub enum Orientation {
     Squarish,
 }
 
-// TODO: implement proper error handling
-pub async fn get_random_photo(
-    access_key: &str,
-    options: GetRandomPhotoOptions,
-) -> Result<DynamicImage> {
-    let metadata: HashMap<String, JsonValue> = unsplash_reqwest_client(access_key)
-        .get(format!("{API_BASE_URL}/photos/random"))
-        .query(&options)
-        .send()
-        .await?
-        .error_for_status()?
-        .json()
-        .await?;
-
-    let url = metadata
-        .get("urls")
-        .ok_or(anyhow!("Metadata missing `urls` key"))?
-        .get("regular")
-        .ok_or(anyhow!("Metadata missing `urls.regular` key"))?
-        .as_str()
-        .ok_or(anyhow!("Invalid data type of `urls.regular` metadata key"))?;
-
-    let image_data = reqwest::get(url).await?.error_for_status()?.bytes().await?;
-
-    let image = ImageReader::new(Cursor::new(image_data))
-        .with_guessed_format()?
-        .decode()?;
-
-    Ok(image)
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Hash)]
+pub struct GetRandomPhotoOptions {
+    pub collections: Option<String>,
+    pub topics: Option<String>,
+    pub username: Option<String>,
+    pub orientation: Option<Orientation>,
 }
